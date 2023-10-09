958529c3aa96e3cee13ffba044783a528b1bcd18
Your Name <me@me.com>
2023-10-09 18:39:10 -0500

    added table

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ let btn2 = document.getElementById('button2');
 let btn3 = document.getElementById('button3');
 let btn4 = document.getElementById('button4');
 
+let table = document.getElementById('table');
 let body = document.getElementById('body');
 let col1 = document.getElementById('col1');
 let col2 = document.getElementById('col2');
 let col3 = document.getElementById('col3');
 
-let title = document.getElementById('title');
+let tbody = document.getElementById('tbody');
 title.display = 'show'  
 
 btn1.addEventListener('click', button1);
@@ -26,13 +27,15 @@ function randomColor() {
     for (let i = 0; i < 6; i++){
         color += letters[Math.floor(Math.random() * 16)];
     }
+    textColorToTable(color);
     return color;
 }
+
+
 
 function button1() {
   body.style.backgroundColor = randomColor();
   //append color name to col1 below button
-  col1.appendChild(document.createTextNode(body.style.backgroundColor));
   }
 
 function titleDisplayToggle () {
@@ -47,4 +50,22 @@ function button4() {
     mainTitle = document.getElementById('name').value;
     title.innerHTML = mainTitle;
     document.getElementById('name').value = '';
+}
+
+function textColorToTable(color) {
+    let row = document.createElement('tr');
+    let cell = document.createElement('td');
+    let cell2 = document.createElement('td');
+    
+    cell.innerHTML = color;
+    row.appendChild(cell);
+    row.appendChild(cell2);
+    cell2.innerHTML = mainTitle;
+    tbody.appendChild(row);
+   
+    for (let i = 0; i < tbody.rows.length; i++) {
+        tbody.rows[i].cells[0].style.color = tbody.rows[i].cells[0].innerHTML;
+        tbody.rows[i].cells[1].style.color = tbody.rows[i].cells[0].innerHTML;
+    }
+ 
 }
