5dcc3a9f127a8492b1914d38c22afe80e062c0c9
Your Name <me@me.com>
2023-10-10 10:57:42 -0500

    corrected comments

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // Get references to buttons by their IDs
-let btn1 = document.getElementById('button1');
-let btn2 = document.getElementById('button2');
-let btn3 = document.getElementById('button3');
-let btn4 = document.getElementById('button4');
+let btn1 = document.getElementById("button1");
+let btn2 = document.getElementById("button2");
+let btn3 = document.getElementById("button3");
+let btn4 = document.getElementById("button4");
 
 // Get references to various HTML elements
-let table = document.getElementById('table');
-let body = document.getElementById('body');
-let col1 = document.getElementById('col1');
-let col2 = document.getElementById('col2');
-let col3 = document.getElementById('col3');
-let tbody = document.getElementById('tbody');
+let table = document.getElementById("table");
+let body = document.getElementById("body");
+let col1 = document.getElementById("col1");
+let col2 = document.getElementById("col2");
+let col3 = document.getElementById("col3");
+let tbody = document.getElementById("tbody");
 
 // Initialize the display property of 'title' to 'show'
-title.style.display = 'show';
+title.style.display = "show";
 
 // Add event listeners to buttons
-btn1.addEventListener('click', button1);
-btn2.addEventListener('click', titleDisplayToggle);
-btn3.addEventListener('click', button3);
-btn4.addEventListener('click', button4);
+btn1.addEventListener("click", button1);
+btn2.addEventListener("click", titleDisplayToggle);
+btn3.addEventListener("click", button3);
+btn4.addEventListener("click", button4);
 
 // Initialize some strings
-let hWorld = 'Hello World';
-let mainTitle = 'Dom Manipulation Example';
+let hWorld = "Hello World";
+let mainTitle = "Dom Manipulation Example";
 
 // Function to generate random color
 function randomColor() {
-  let color = '#';
-  let letters = '0123456789ABCDEF';
-  for (let i = 0; i < 6; i++){
+  let color = "#";
+  let letters = "0123456789ABCDEF";
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   textColorToTable(color);
@@ -44,16 +44,41 @@ function button1() {
 
 // Function to toggle the display of 'title'
 function titleDisplayToggle() {
-  title.style.display === 'none' ? title.style.display = 'block' : title.style.display = 'none';
+  title.style.display === "none"
+    ? (title.style.display = "block")
+    : (title.style.display = "none");
 }
 
 // Function to toggle the innerHTML of 'title'
 function button3() {
-  title.innerHTML === mainTitle ? title.innerHTML = hWorld : title.innerHTML = mainTitle;
+  title.innerHTML === mainTitle
+    ? (title.innerHTML = hWorld)
+    : (title.innerHTML = mainTitle);
 }
 
 // Function to set 'title' to user input and clear the input field
 function button4() {
-  mainTitle = document.getElementById('name').value;
+  mainTitle = document.getElementById("name").value;
   title.innerHTML = mainTitle;
-  document.getElementById('name').value
+  document.getElementById("name").value = "";
+}
+
+// Function to add a new row to the table and set its color
+function textColorToTable(color) {
+  let row = document.createElement("tr");
+  let cell = document.createElement("td");
+  let cell2 = document.createElement("td");
+
+  row.classList.add("table-transition");
+  cell.innerHTML = color;
+  row.appendChild(cell);
+  row.appendChild(cell2);
+  cell2.innerHTML = mainTitle;
+  tbody.appendChild(row);
+
+  // Loop to set the text color for all rows in the table
+  for (let i = 0; i < tbody.rows.length; i++) {
+    tbody.rows[i].cells[0].style.color = tbody.rows[i].cells[0].innerHTML;
+    tbody.rows[i].cells[1].style.color = tbody.rows[i].cells[0].innerHTML;
+  }
+}
